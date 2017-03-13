c3f102b1de2ee67688fa84c4df4ca7ecb3ae447c
vol4tim <vol4tim@gmail.com>
2017-03-13 10:33:03 +0400

    contracts main net

--- dapp/src/utils/address.js
+++ dapp/src/utils/address.js
@@ -1,12 +1,7 @@
 export default {
-  BuilderCore: '0x5b12ae5c4f045992fab376f275e7d4150f15429c',
-  BuilderOperator: '0x33b36ddced698de14f07f4c731037a984bb48bfd',
-  BuilderAuditor: '0xbd2bc26b61c216884797ba8f45ad5ee3bbe5a654',
-  // BuilderIssuerLedger: '0x4ec7e0e3e37a355327be99fc216d9908b1a9275a',
-  BuilderIssuerLedger: '0xa322108a7aa4eb5fae749ea35284160e9dc93294',
-  BuilderTokenEmission: '0x01fbc768387ce4fd447e3385052166a795cacc95',
-  BuilderCommitment: '0xE4f6391b78cF4011185102ED47748114723B64c0',
-  BuilderInsuranceHolder: '0x6656dddd401acd91d79250fe0b67dfb443dc9c31',
-  BuilderTokenEmissionACL: '0x2a06987cd0fe3f71adb5bc762a9ad2c94d1add22',
-  BuilderTokenWithValidityPeriod: '0x8333ae1f2c1b194209b07689ed826664512cc2ba'
+  Factory: '0xFE011b26545a5B6888b9932b6D8e0D4D563538C5',
+  BuilderMarket: '0x4ec7e0e3e37a355327be99fc216d9908b1a9275a',
+  BuilderDocs: '0x1acc3cc17e01e6df9e8d2486c01058e3d0b161c6',
+  BuilderIssuerLedger: '0x9d79f89924010d71e699f837a57b65a54aa84e1f',
+  BuilderTokenWithValidityPeriod: '0x00178516b4293fb6fb9c28325f0e92474ffde046'
 }
