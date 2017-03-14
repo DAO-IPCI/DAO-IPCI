c003e8703fa5898e70d9a7931be9a5c9e0b97e44
vol4tim <vol4tim@gmail.com>
2017-03-14 09:10:36 +0400

    enable create module from operator

--- dapp/src/shared/dao/main/components/block.js
+++ dapp/src/shared/dao/main/components/block.js
@@ -9,14 +9,10 @@ const Block = (props) => {
   return (<div className="panel panel-default">
     <div className="panel-heading">
       {name}
-      {(role === 'operator' && (type === 'token' || type === 'market' || type === 'docs')) &&
-        <div className="btn-group pull-right" style={{ marginBottom: 10 }}>
-          <Link to={'/dao/create/' + type} className="btn btn-info btn-xs"><span className="glyphicon glyphicon-plus" /></Link>
-        </div>
-      }
-      {(role === 'operator') &&
+      {role === 'operator' &&
         <div className="btn-group pull-right" style={{ marginBottom: 10 }}>
           <Link to={'/dao/link/' + type} className="btn btn-info btn-xs"><span className="glyphicon glyphicon-link" /></Link>
+          <Link to={'/dao/create/' + type} className="btn btn-info btn-xs"><span className="glyphicon glyphicon-plus" /></Link>
         </div>
       }
     </div>
