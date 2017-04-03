387e7778466307040fd4502151422fd19f6a6f00
vol4tim <vol4tim@gmail.com>
2017-04-03 15:56:52 +0400

    add decimals builder

--- dapp/src/utils/address.js
+++ dapp/src/utils/address.js
@@ -2,5 +2,5 @@ export default {
   Factory: '0xFE011b26545a5B6888b9932b6D8e0D4D563538C5',
   BuilderMarket: '0x4ec7e0e3e37a355327be99fc216d9908b1a9275a',
   BuilderDocs: '0x1acc3cc17e01e6df9e8d2486c01058e3d0b161c6',
-  BuilderIssuerLedger: '0x62797a6229b96e64d9f627894a3ef94663f0114c'
+  BuilderIssuerLedger: '0x32eccf71c0eff709cd11f704767825d0f8e375e7'
 }
