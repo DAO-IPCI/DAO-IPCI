68dc55e35a15c3897a9c5cd660986c871f8c09eb
vol4tim <vol4tim@gmail.com>
2017-05-29 13:54:12 +0400

    change ipfs host

--- dapp/src/config/config.js
+++ dapp/src/config/config.js
@@ -1,6 +1,6 @@
-export const IPFS_HOST = 'ipci.io'
+export const IPFS_HOST = '52.166.18.229'
 export const IPFS_PORT = '5001'
-export const IPFS_PROTOCOL = 'https'
+export const IPFS_PROTOCOL = 'http'
 export const PROGRAMMS = [
   {
     address: '0x9ee8b04a4a5e301eabdb791fb6ed4e7d815cf90d',
