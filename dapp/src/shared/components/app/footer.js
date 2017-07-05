4657eb0a2db2b42c2ffdf740b4d5045e2543406b
vol4tim <vol4tim@gmail.com>
2017-07-05 10:27:45 +0400

    upt text

--- dapp/src/shared/components/app/footer.js
+++ dapp/src/shared/components/app/footer.js
@@ -4,7 +4,7 @@ export default function () {
   return (
     <footer className="footer">
       <div className="container">
-        <p><strong>Disclaimer:</strong> DApp IPCI has started in the summer of 2016 and is in active development. If you find bugs or you have questions, please write on <a href="mailto:help@ipci.io">help@ipci.io</a> </p>
+        <p><strong>Disclaimer:</strong> DApp IPCI has started in the summer of 2016 and is in the stage of development. If you find bugs or have questions, please contact <a href="mailto:help@ipci.io">help@ipci.io</a> </p>
       </div>
     </footer>
   )
