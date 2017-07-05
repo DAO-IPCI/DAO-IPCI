65324fc0205dd83e682f0b2140b0fa0272d6c915
vol4tim <vol4tim@gmail.com>
2017-07-05 10:30:37 +0400

    remove file test

--- dapp/src/config/address_main.js
+++
@@ -1,7 +0,0 @@
-export default {
-  Factory: '0xFE011b26545a5B6888b9932b6D8e0D4D563538C5',
-  BuilderMarket: '0x4ec7e0e3e37a355327be99fc216d9908b1a9275a',
-  BuilderDocs: '0x1acc3cc17e01e6df9e8d2486c01058e3d0b161c6',
-  BuilderIssuerLedger: '0x9d79f89924010d71e699f837a57b65a54aa84e1f',
-  BuilderTokenWithValidityPeriod: '0x00178516b4293fb6fb9c28325f0e92474ffde046'
-}
